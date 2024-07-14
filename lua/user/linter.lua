d31c699dbda92d67c8c887b02f113dab897adc25
Alexei Torgashov <alexei.torgashov@yahoo.de>
2024-07-14 13:48:02 +0200

    replace eslint and prettier with biome and copilot with codeium

--- lua/user/linter.lua
+++ lua/user/linter.lua
@@ -2,35 +2,23 @@
 local formatters = require("lvim.lsp.null-ls.formatters")
 
 formatters.setup({
-	{ command = "stylua" },
-	{
-		command = "prettierd",
-		filetypes = { "javascript", "typescript", "typescriptreact", "html", "tailwindcss", "json" },
-	},
-	{
-		command = "eslint_d",
-		filetypes = { "typescript", "typescriptreact", "javascript", "svelte" },
-	},
-	{
-		command = "latexindent",
-		filetypes = { "tex", "latex" },
-		extra_args = { "-l", vim.fn.expand("%:p:h") .. "/latexindent.yaml", "-m" },
-	},
-	{ command = "black", filetypes = { "python" } },
-	{ command = "taplo", filetypes = { "toml" } },
-	{ command = "beautysh", filetypes = { "bash", "csh", "ksh", "sh", "zsh" } },
+    { command = "stylua" },
+    {
+        command = "latexindent",
+        filetypes = { "tex", "latex" },
+        extra_args = { "-l", vim.fn.expand("%:p:h") .. "/latexindent.yaml", "-m" },
+    },
+    { command = "black",    filetypes = { "python" } },
+    { command = "taplo",    filetypes = { "toml" } },
+    { command = "beautysh", filetypes = { "bash", "csh", "ksh", "sh", "zsh" } },
 })
 
 local linters = require("lvim.lsp.null-ls.linters")
 
 linters.setup({
-	{
-		command = "eslint_d",
-		filetypes = { "javascript", "typescript", "typescriptreact", "svelte" },
-	},
-	{
-		command = "flake8",
-		filetypes = { "python" },
-		extra_args = { "--max-line-length", "80", "--select", "B950", "--ignore", "E203,E501,W503" },
-	},
+    {
+        command = "flake8",
+        filetypes = { "python" },
+        extra_args = { "--max-line-length", "80", "--select", "B950", "--ignore", "E203,E501,W503" },
+    },
 })
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,74 +1,72 @@
 -- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
-	-- GitHub copilot
-	{
-		"zbirenbaum/copilot.lua",
-		event = "InsertEnter",
-		config = function()
-			require("copilot").setup({
-				suggestion = {
-					auto_trigger = true,
-					keymap = {
-						accept = "<M-m>",
-					},
-				},
-			})
-		end,
-	},
-	--
-	--git diff in a single tabpage
-	{
-		"sindrets/diffview.nvim",
-		event = "BufRead",
-	},
-	--
-	-- highlight and search for todo comments
-	{
-		"folke/todo-comments.nvim",
-		event = "BufRead",
-		config = function()
-			require("todo-comments").setup()
-		end,
-	},
-	--
-	-- mappings to delete, change and add surroundings
-	{
-		"tpope/vim-surround",
+    -- Copilot
+    {
+        "Exafunction/codeium.vim",
+        config = function()
+            -- Change '<C-g>' here to any keycode you like.
+            vim.keymap.set('i', '<M-m>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+            vim.keymap.set('i', '<M-j>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+                { expr = true, silent = true })
+            vim.keymap.set('i', '<M-k>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+                { expr = true, silent = true })
+            vim.keymap.set('i', '<M-y>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+        end,
+    },
+    --git diff in a single tabpage
+    {
+        "sindrets/diffview.nvim",
+        event = "BufRead",
+    },
+    --
+    -- highlight and search for todo comments
+    {
+        "folke/todo-comments.nvim",
+        event = "BufRead",
+        config = function()
+            require("todo-comments").setup()
+        end,
+    },
+    --
+    -- mappings to delete, change and add surroundings
+    {
+        "tpope/vim-surround",
 
-		-- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
-		-- setup = function()
-		--  vim.o.timeoutlen = 500
-		-- end
-	},
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"lua-language-server",
-				"html-lsp",
-				"prettierd",
-				"eslint_d",
-				"stylua",
-				"tailwindcss-language-server",
-				"svelte-language-server",
-				"prisma-language-server",
-				"emmet-ls",
-				"json-lsp",
-				"pyright",
-				"texlab",
-				"rust-analyzer",
-				"flake8",
-				"black",
-			},
-		},
-	},
-	{ "lervag/vimtex" },
-	{
-		"andrewferrier/wrapping.nvim",
-		config = function()
-			require("wrapping").setup()
-		end,
-	},
+        -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+        -- setup = function()
+        --  vim.o.timeoutlen = 500
+        -- end
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "lua-language-server",
+                "html-lsp",
+                -- "prettierd",
+                -- "eslint_d",
+                "stylua",
+                "tailwindcss-language-server",
+                "svelte-language-server",
+                "prisma-language-server",
+                "emmet-ls",
+                "json-lsp",
+                "pyright",
+                "texlab",
+                "rust-analyzer",
+                "flake8",
+                "black",
+                "biome"
+            },
+        },
+    },
+    { "lervag/vimtex" },
+    {
+        "andrewferrier/wrapping.nvim",
+        config = function()
+            require("wrapping").setup()
+        end,
+    },
 }
 
 -- Can not be placed into the config method of the plugins.
